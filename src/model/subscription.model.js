31ba41735303fa2ca56034e731a0581521ce5ec7
bttrainees123 <bttrainees1@gmail.com>
2025-08-14 17:22:54 +0530

    payment gateway

--- src/model/subscription.model.js
+++ src/model/subscription.model.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 const subscriptionSchema = require("../schema/subscription.schema");
-const subscriptionModel = mongoose.model('subscripts',subscriptionSchema);
+const subscriptionModel = mongoose.model('subscription',subscriptionSchema);
 module.exports = subscriptionModel;
